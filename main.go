a0cf0a736b8defc93a300bef149a23bb8a2099c7
elbashery <ahmedalbachiry@gmail.com>
2020-06-12 11:45:25 +0300

    rename module

--- main.go
+++ main.go
@@ -4,28 +4,29 @@ import (
 	"fmt"
 
 	"github.com/elbashery/tweets/db"
-	"github.com/elbashery/tweets/routes"
+	"github.com/elbashery/tweets/tweet"
 	"github.com/gofiber/fiber"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
 func initdb() {
+	fmt.Println("hello")
 	var err error
 	db.DBconn, err = gorm.Open("sqlite3", "tweets.db")
 	if err != nil {
 		panic("error when open database")
 	}
 	fmt.Println("database successfully connection")
-	db.DBconn.AutoMigrate(&routes.Tweet{})
+	db.DBconn.AutoMigrate(&tweet.Tweet{})
 	fmt.Println("database megrated")
 }
 func setUpRouts(app *fiber.App) {
-	app.Get("/tweet/:id", routes.GetOne)
-	app.Get("/tweets", routes.GetAll)
-	app.Put("/tweet/:id", routes.Edite)
-	app.Post("/tweet/:id", routes.Edite)
-	app.Delete("/tweet/:id", routes.Remove)
+	app.Get("/tweet/:id", tweet.GetOne)
+	app.Get("/tweets", tweet.GetAll)
+	app.Put("/tweet/:id", tweet.Edite)
+	app.Post("/tweet/:id", tweet.Edite)
+	app.Delete("/tweet/:id", tweet.Remove)
 }
 func main() {
 	app := fiber.New()
