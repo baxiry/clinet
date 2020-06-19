0c7cdf38f63ed301e864ebb2e89b4e96eb082830
elbashery <ahmedalbachiry@gmail.com>
2020-06-19 17:42:53 +0300

    fix some bug

--- assets/app.js
+++ assets/app.js
@@ -2,7 +2,7 @@
 var app1 = new Vue({
   el: '#app1',
   data: {
-      message: 'Ok Hello Vue!'
+      message: 'ok hello vue!'
   }
 })
 
@@ -10,6 +10,13 @@ var app2 = new Vue({
     el: '#app2',
     data: {
        message: 'som discreptYou loaded this page on '+
-        new Date().toLocaleString(),
+        new Date().toLocaleString()
+    }
+})
+
+var app3 = new Vue({
+    el: '#app3',
+    data: {
+        toggle: true,
     }
 })
