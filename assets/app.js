7e8191a6ccdb4a8538632eab543965ba5b3eaf7e
elbashery <ahmedalbachiry@gmail.com>
2020-06-29 17:07:03 +0300

    add delete to tweet app

--- assets/app.js
+++ assets/app.js
@@ -20,13 +20,13 @@ var Get_tweets = new Vue({
 
 // post new tweet
 var Post_tweets = new Vue({
-    el: '#post_tweets',
+    el: '#post_tweet',
     data:{
         nil : false,
         tweet: {title:"new topic", }
     },
     methods: {
-        post_tweets: function() { // undefined
+        post_tweet: function() { // undefined
             //console.log(this.tweet.body == undefined )
             if (this.tweet.body != undefined) {
                 this.nil = false
@@ -53,9 +53,21 @@ var Post_tweets = new Vue({
     }
 })
 
-var emptyt = new Vue({
-    //el : '#nil',
+var delete_tweet = new Vue({
+    el : '#del_tweet',
+    data: {
+        id: 20,
+    },
     methods:{
-
+        delete_tweet: function() { // undefined
+            // delete tweeti
+            axios.delete('http://localhost:9000/tweet/'+this.id)
+            //.then(function (response) {
+                //console.log(response.data)
+            //})
+            .catch(function (error) {
+                 console.log("Have an: "+ error)
+            });
+        } 
     }
 })
