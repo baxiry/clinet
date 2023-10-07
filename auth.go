16b7f2a420a26a010c1d75b54be0e710f513a2fb
bashery <ahmedalbachiry@gmail.com>
2023-10-07 11:52:31 +0300

    clinet is client of network

--- auth.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "fmt"
-
-func auth() {
-	fmt.Println("Auth pkg")
-}
--- main.go
+++ main.go
@@ -1,36 +1,48 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 
-	"github.com/bashery/tweets/dbs"
-	"github.com/bashery/tweets/routs"
-	"github.com/bashery/tweets/tweet"
-	"github.com/gofiber/fiber"
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"github.com/gorilla/websocket"
 )
 
-func initdb() {
-	var err error
-	dbs.Conn, err = gorm.Open("sqlite3", "../.tweets.db")
-	if err != nil {
-		fmt.Println(err)
-		panic("error when open database")
-	}
-	//fmt.Println("database successfully connection")
-	dbs.Conn.AutoMigrate(&tweet.Tweet{})
-	//dbs.Conn.AutoMigrate(&user.Profile{})
-	//fmt.Println("database megrated")
-}
+var dbName string = "test"
 
 func main() {
-	//auth()
-	app := fiber.New()
-	app.Static("/", "./assets", fiber.Static{ByteRange: true})
-	initdb()
-	defer dbs.Conn.Close()
-	routs.SetUpRouts(app)
+	socketUrl := "ws://localhost:1111/ws"
 
-	app.Listen(8000)
+	conn, _, err := websocket.DefaultDialer.Dial(socketUrl, nil)
+	if err != nil {
+		fmt.Println("Error! :", err)
+		return
+	}
+	defer conn.Close()
+
+	// handle std input
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print(dbName + "> ")
+		inputQuery, _ := reader.ReadString('\n')
+		if inputQuery == "" {
+			println("zeroval")
+			continue
+		}
+
+		// Send an echo packet every second
+		err = conn.WriteMessage(websocket.TextMessage, []byte(inputQuery))
+		if err != nil {
+			fmt.Println("Error! :", err)
+			return
+		}
+
+		_, msg, err := conn.ReadMessage()
+		if err != nil {
+			fmt.Println("Error in receive:", err)
+			return
+		}
+		fmt.Println(string(msg))
+
+	}
 }
